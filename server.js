c0975712dcbe57c6d079dc0d38b24162cda66d1b
Ismael Moral <jastertdc@icloud.com>
2015-06-17 22:08:06 +0200

    Added /public folder

---
+++ server.js
@@ -0,0 +1,31 @@
+
+// Include section.
+var express       = require ('express'),
+    bodyParser    = require ('body-parser'),
+    methodOver    = require ('method-override'),
+    morgan        = require ('morgan'),
+    mongoose      = require ('mongoose'),
+    database      = require ('./config/database'),
+    app           = express ();
+
+// Port.
+var port          = process.env.PORT || 1557;
+
+// App config.
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOver('X-HTTP-Method-Override'));
+app.use(morgan ('dev'));
+
+// Database connection.
+mongoose.connect (database.url, function (err, rs){
+  if (err)
+    res.send (err);
+});
+
+// Routes.
+
+// Listening port.
+app.listen (port);
