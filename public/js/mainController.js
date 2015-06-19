29864366be9f1eae6a527f4b760009ac5b951842
Ismael Moral <jastertdc@icloud.com>
2015-06-19 11:16:45 +0200

    Added angular directive

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -2,6 +2,13 @@
 
 var app = angular.module ('angular-blog', []);
 
+app.directive ('menuBar', function (){
+  return {
+    restrict        : 'E',
+    templateUrl     : '../templates/menu-bar.html'
+  };
+});
+
 function mainController ($scope, $http){
   $scope.formData = {};
 
